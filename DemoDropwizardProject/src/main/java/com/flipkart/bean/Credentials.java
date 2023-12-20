0ae5cba7606f6886ffcdffaddc382c38dc47fbe3
Harsh <harshagrawal2412@gmail.com>
2023-12-20 11:45:27 +0530

    final commit

---
+++ DemoDropwizardProject/src/main/java/com/flipkart/bean/Credentials.java
@@ -0,0 +1,21 @@
+package com.flipkart.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Credentials {
+    private String user;
+    private String password;
+	public Object getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
